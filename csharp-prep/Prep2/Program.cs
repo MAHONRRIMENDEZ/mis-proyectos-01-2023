ee1abe43ca4a1d8c4651a421d51de4ddc2c61a19
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 21:11:24 -0500

    Prep 2 advanced

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+     Console.Write("What is your current grade? ");
+     String userGrade = Console.ReadLine();
+     
+     if (userGrade > 90 && userGrade < 101);
+     {
+        UserGradeLetter == "A"
+        Console.Write($"your grade is {UserGradeLetter}");   
+     }
+    
+    
     }
 }
