189889226f6f9d4c045601c50d0a2118b0b50c63
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 21:44:02 -0500

    prep 2 completed again

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,38 +8,38 @@ class Program
         String userGrade = Console.ReadLine();
         int percent = int.Parse(userGrade);
      
-            string userGradeLetter = "";
+        string userGradeLetter = "";
 
-            if (percent >= 90 && percent < 101);
-            {
-                userGradeLetter = "A";
-            } 
-            else if (percent >= 80)
-            {
-                 userGradeLetter = "B";
-            }
-            else if (percent >= 70)
-            {
-                userGradeLetter = "C";
-            }
-            else if (percent >= 60)
-            {
-                userGradeLetter = "D";
-            }
-            else
-            {
-                userGradeLetter = "F";
-            }
+        if (percent >= 90 && percent < 101);
+        {
+            userGradeLetter = "A";
+        } 
+        else if (percent >= 80)
+        {
+            userGradeLetter = "B";
+        }
+        else if (percent >= 70)
+        {
+            userGradeLetter = "C";
+        }
+        else if (percent >= 60)
+        {
+            userGradeLetter = "D";
+        }
+        else
+        {
+            userGradeLetter = "F";
+        }
 
-            Console.WriteLine($"Your grade is: {letter}");
-            
-            if (percent >= 70)
-            {
-                Console.WriteLine("Congrats You've approved!");
-            }
-            else
-            {
-                Console.WriteLine("Better luck next time!");
-            }
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats You've approved!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
